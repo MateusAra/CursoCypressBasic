0191861e1435e3baa2d123a9798bd6e6fcbea4b8
MateusAra <mateus19araujo@hotmail.com>
2022-11-26 10:52:07 -0300

    ..............

--- cypress/e2e/CAT-TAT.spec.cy.js
+++ cypress/e2e/CAT-TAT.spec.cy.js
@@ -11,8 +11,8 @@ describe('Central de Atendimento ao cliente TAT', function(){
         const longText = 'O comando cy.get() passa adiante ao próximo comando o elemento encontrado, e outros comandos, tais como o .type() e click() (dentre outros), também passam adiante o mesmo elemento, possibilitando encadearmos novos comandos, por exemplo, para fazer uma verificação.'
         cy.get('#firstName').type('Mateus')
         cy.get('#lastName').type('Araujo')
-        cy.get('#email').type('mateus@gmail,com', {log: false})
-        cy.get('#open-text-area').type(longText, {log: false})
+        cy.get('#email').type('mateus@gmail,com')
+        cy.get('#open-text-area').type(longText)
         cy.contains('button', 'Enviar').click()
         cy.get('.success').should('be.visible')
     });
